d34013565d01f136f833a5ac0a095aa8b6472291
Tit Petric <black@scene-si.org>
2016-06-06 11:48:49 +0200

    import bootstrap package

---
+++ api-foundations/chapter6/bootstrap/now.go
@@ -0,0 +1,13 @@
+package bootstrap
+
+import "time"
+
+var StartTime float64
+
+func Now() float64 {
+	myTime := float64(time.Now().UnixNano()) / 1000000.0
+	if StartTime < 0.000001 {
+		StartTime = myTime
+	}
+	return myTime - StartTime
+}
---
+++ api-foundations/chapter6/bootstrap/redis.go
@@ -0,0 +1,14 @@
+package bootstrap
+
+import "time"
+import "github.com/garyburd/redigo/redis"
+
+var (
+	connectTimeout = redis.DialConnectTimeout(time.Second)
+	readTimeout    = redis.DialReadTimeout(time.Second)
+	writeTimeout   = redis.DialWriteTimeout(time.Second)
+)
+
+func GetRedis() (redis.Conn, error) {
+	return redis.Dial("tcp", "redis:6379", connectTimeout, readTimeout, writeTimeout)
+}
