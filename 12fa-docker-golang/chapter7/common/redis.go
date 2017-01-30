90ca12165e21a17cc1083572519653a9606c80ad
Tit Petric <black@scene-si.org>
2017-01-30 21:16:18 +0100

    factor 3 compliance: add functional options to redis, pass config from main

--- 12fa-docker-golang/chapter7/common/redis.go
+++ 12fa-docker-golang/chapter7/common/redis.go
@@ -3,12 +3,87 @@ package common
 import "time"
 import "github.com/garyburd/redigo/redis"
 
+type Redis struct {
+	conn                                      redis.Conn
+	protocol, address                         string
+	connectTimeout, readTimeout, writeTimeout time.Duration
+}
+
+type RedisOption struct {
+	f func(*Redis)
+}
+
+func RedisProtocol(protocol string) RedisOption {
+	return RedisOption{func(do *Redis) {
+		do.protocol = protocol
+	}}
+}
+
+func RedisAddress(address string) RedisOption {
+	return RedisOption{func(do *Redis) {
+		do.address = address
+	}}
+}
+
+func RedisConnectTimeout(timeout time.Duration) RedisOption {
+	return RedisOption{func(do *Redis) {
+		do.connectTimeout = timeout
+	}}
+}
+
+func RedisReadTimeout(timeout time.Duration) RedisOption {
+	return RedisOption{func(do *Redis) {
+		do.readTimeout = timeout
+	}}
+}
+func RedisWriteTimeout(timeout time.Duration) RedisOption {
+	return RedisOption{func(do *Redis) {
+		do.writeTimeout = timeout
+	}}
+}
+
 var (
-	connectTimeout = redis.DialConnectTimeout(time.Second)
-	readTimeout    = redis.DialReadTimeout(time.Second)
-	writeTimeout   = redis.DialWriteTimeout(time.Second)
+	redisConnections map[string]*Redis = make(map[string]*Redis, 0)
 )
 
-func GetRedis() (redis.Conn, error) {
-	return redis.Dial("tcp", "redis:6379", connectTimeout, readTimeout, writeTimeout)
+func NewRedis(options ...RedisOption) *Redis {
+	redis := &Redis{}
+	redis.conn = nil
+	redis.protocol = "tcp"
+	redis.address = "redis:6379"
+	redis.connectTimeout = time.Second
+	redis.readTimeout = time.Second
+	redis.writeTimeout = time.Second
+	for _, option := range options {
+		option.f(redis)
+	}
+	return redis
+}
+
+func (t *Redis) Save(names ...string) {
+	connection := "default"
+	for _, name := range names {
+		connection = name
+	}
+	redisConnections[connection] = t
+}
+
+func GetRedis(names ...string) (redis.Conn, error) {
+	var err error
+	connection := "default"
+	for _, name := range names {
+		connection = name
+	}
+	r, ok := redisConnections[connection]
+	if !ok {
+		r = NewRedis()
+		r.Save(connection)
+	}
+	if r.conn == nil {
+		connectTimeout := redis.DialConnectTimeout(r.connectTimeout)
+		readTimeout := redis.DialReadTimeout(r.readTimeout)
+		writeTimeout := redis.DialWriteTimeout(r.writeTimeout)
+		r.conn, err = redis.Dial(r.protocol, r.address, connectTimeout, readTimeout, writeTimeout)
+	}
+	return r.conn, err
 }
--- 12fa-docker-golang/chapter7/main.go
+++ 12fa-docker-golang/chapter7/main.go
@@ -2,22 +2,32 @@ package main
 
 import (
 	"app/api"
+	"app/common"
 	"flag"
 	"log"
 	"net/http"
 )
 
-var apiTwitter api.Twitter
-
-var port = flag.String("port", "8080", "Port for server")
-
 func main() {
+	// set up flags
+	var (
+		port  = flag.String("port", "8080", "Listen port for server")
+		redis = flag.String("redis", "redis:6379", "Redis address (host:port)")
+	)
 	flag.Parse()
 
+	// set up config
+	r := common.NewRedis(common.RedisAddress(*redis))
+	r.Save()
+
+	// set up twitter api
+	apiTwitter := api.Twitter{}
 	apiTwitter.Register()
 
+	// serve static assets
 	http.Handle("/", http.FileServer(http.Dir("./public_html")))
 
+	// start up http server
 	log.Printf("Ready and listening on port " + *port + "\n")
 	if err := http.ListenAndServe(":"+*port, nil); err != nil {
 		panic(err)
