45fe96a71f4f8282690c4f1eb1ca5989d81b62b7
Tit Petric <black@scene-si.org>
2017-01-30 21:23:25 +0100

    dump protocol support to have file be more compact

--- 12fa-docker-golang/chapter7/common/redis.go
+++ 12fa-docker-golang/chapter7/common/redis.go
@@ -5,18 +5,12 @@ import "github.com/garyburd/redigo/redis"
 
 type Redis struct {
 	conn                                      redis.Conn
-	protocol, address                         string
+	address                         string
 	connectTimeout, readTimeout, writeTimeout time.Duration
 }
 
 type RedisOption struct {
 	f func(*Redis)
-}
-
-func RedisProtocol(protocol string) RedisOption {
-	return RedisOption{func(do *Redis) {
-		do.protocol = protocol
-	}}
 }
 
 func RedisAddress(address string) RedisOption {
@@ -49,7 +43,6 @@ var (
 func NewRedis(options ...RedisOption) *Redis {
 	redis := &Redis{}
 	redis.conn = nil
-	redis.protocol = "tcp"
 	redis.address = "redis:6379"
 	redis.connectTimeout = time.Second
 	redis.readTimeout = time.Second
@@ -83,7 +76,7 @@ func GetRedis(names ...string) (redis.Conn, error) {
 		connectTimeout := redis.DialConnectTimeout(r.connectTimeout)
 		readTimeout := redis.DialReadTimeout(r.readTimeout)
 		writeTimeout := redis.DialWriteTimeout(r.writeTimeout)
-		r.conn, err = redis.Dial(r.protocol, r.address, connectTimeout, readTimeout, writeTimeout)
+		r.conn, err = redis.Dial("tcp", r.address, connectTimeout, readTimeout, writeTimeout)
 	}
 	return r.conn, err
 }
