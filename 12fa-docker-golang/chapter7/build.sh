d3ff12f10082164980b8ae531e98dc0a48c347db
Tit Petric <black@scene-si.org>
2017-02-07 00:59:58 +0100

    update build/run to use golang:1.8-alpine

--- 12fa-docker-golang/chapter7/build.sh
+++ 12fa-docker-golang/chapter7/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd $(dirname $(readlink -f $0))
-if [ ! -d "build/public_html" ]; then
-	mkdir -p build/public_html
-fi
+rm -rf build
+mkdir -p build/public_html
 rsync -a public_html/ build/public_html/
-docker run --net=party --rm -it -p 8080:8080 -v `pwd`:/go/src/app -w /go/src/app golang go build -o build/gotwitter main.go
+echo "Building app"
+docker run --net=party --rm -it -p 8080:8080 -v `pwd`:/go/src/app -w /go/src/app golang:1.8-alpine go build -o build/gotwitter main.go
 rsync -a build/ ../chapter8/app/
