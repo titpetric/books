17bb1e9e8f54ca8f15f7ed404c7bfeb132618eb2
Tit Petric <black@scene-si.org>
2017-02-08 23:05:10 +0100

    name my repo

--- 12fa-docker-golang/chapter7/build.sh
+++ 12fa-docker-golang/chapter7/build.sh
@@ -7,4 +7,4 @@ echo "Building app"
 docker run --net=party --rm -it -v `pwd`:/go/src/app -w /go/src/app golang:1.8-alpine go build -o build/gotwitter main.go
 rsync -a build/ ../chapter8/app/
 echo "Building docker container"
-docker build --rm --no-cache=true -t gotwitter .
+docker build --rm --no-cache=true -t titpetric/gotwitter .
