e764c01176131bd96114ba390cd95090a118950e
Tit Petric <black@scene-si.org>
2017-02-08 15:15:38 +0100

    Build docker container as well

--- 12fa-docker-golang/chapter7/build.sh
+++ 12fa-docker-golang/chapter7/build.sh
@@ -4,5 +4,7 @@ rm -rf build
 mkdir -p build/public_html
 rsync -a public_html/ build/public_html/
 echo "Building app"
-docker run --net=party --rm -it -p 8080:8080 -v `pwd`:/go/src/app -w /go/src/app golang:1.8-alpine go build -o build/gotwitter main.go
+docker run --net=party --rm -it -v `pwd`:/go/src/app -w /go/src/app golang:1.8-alpine go build -o build/gotwitter main.go
 rsync -a build/ ../chapter8/app/
+echo "Building docker container"
+docker build --rm --no-cache=true -t gotwitter .
