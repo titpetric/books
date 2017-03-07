b8e030bc509d644baa77eb873593ac7cc573a8f0
Tit Petric <black@scene-si.org>
2017-03-07 01:02:47 +0100

    static compilation

---
+++ 12fa-docker-golang/chapter7/build_static.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd $(dirname $(readlink -f $0))
+rm -rf build
+mkdir -p build/public_html
+rsync -a public_html/ build/public_html/
+echo "Building app"
+docker run --net=party -e CGO_ENABLED=0 --rm -it -v `pwd`:/go/src/app -w /go/src/app golang:1.8-alpine go build -o build/gotwitter -x --ldflags='-extldflags "-static"' main.go
+rsync -a build/ ../chapter8/app/
+exit
+echo "Building docker container"
+docker build --rm --no-cache=true -t titpetric/gotwitter .
