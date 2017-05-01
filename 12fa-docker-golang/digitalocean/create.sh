70f084551f147598352e20a2fffeaad0c9368b84
Tit Petric <black@scene-si.org>
2017-05-01 08:34:30 +0200

    whitespace

--- 12fa-docker-golang/digitalocean/create.sh
+++ 12fa-docker-golang/digitalocean/create.sh
@@ -2,7 +2,11 @@
 CHECK=$(doctl compute droplet list asx --format ID --no-header | wc -l)
 if [ "$CHECK" == "0" ]; then
 	echo "Creating ASX droplet"
-	doctl compute droplet create asx --image docker-16-04 --size 2gb --region ams3 --ssh-keys 01:23:45:67:89:0a:bc:de:fe:dc:ba:98:76:54:32:10 -v
+	doctl compute droplet create asx -v \
+		--image docker-16-04 \
+		--size 2gb \
+		--region ams3 \
+		--ssh-keys 01:23:45:67:89:0a:bc:de:fe:dc:ba:98:76:54:32:10
 else
 	echo "Droplet ASX already running"
 fi
