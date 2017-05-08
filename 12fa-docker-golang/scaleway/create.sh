b082cb6a707206f4595ecbc102dbd87bf7491057
Tit Petric <black@scene-si.org>
2017-05-08 17:02:54 +0200

    bit of a convoluted add-tags example

---
+++ 12fa-docker-golang/scaleway/create.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+source swarm/include/common.sh
+echo "Creating server asx"
+scw _patch $(scw create --name=asx --commercial-type=X64-2GB ubuntu-xenial | sed 's/\-.*//g') tags="docker"
+echo "Starting server asx"
+scw start -w asx
+ADDR=$(./get-ip.sh)
+scp scripts/* $ADDR:/root
+ssh $ADDR /root/bootstrap.sh
+echo "Finished."
