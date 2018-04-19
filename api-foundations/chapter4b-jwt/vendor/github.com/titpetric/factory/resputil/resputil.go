3bb142db08e1eef5fe5ed59f930bcea6570703fa
Tit Petric <black@scene-si.org>
2018-04-19 14:43:15 +0200

    update vendor

--- api-foundations/chapter4b-jwt/vendor/github.com/titpetric/factory/resputil/resputil.go
+++ api-foundations/chapter4b-jwt/vendor/github.com/titpetric/factory/resputil/resputil.go
@@ -13,14 +13,22 @@ type stackTracer interface {
 	StackTrace() errors.StackTrace
 }
 
+type successMessage struct {
+	Success struct {
+		Message string `json:"message"`
+	} `json:"success"`
+}
+
+type errorMessage struct {
+	Error struct {
+		Message string `json:"message"`
+		Trace   string `json:"trace,omitempty"`
+	} `json:"error"`
+}
+
 // Error returns a structured error for API responses
-func Error(err error) interface{} {
-	response := struct {
-		Error struct {
-			Message string `json:"message"`
-			Trace   string `json:"trace,omitempty"`
-		} `json:"error"`
-	}{}
+func Error(err error) errorMessage {
+	response := errorMessage{}
 	// add stack trace to the response if available
 	terr, ok := errors.Cause(err).(stackTracer)
 	if ok {
@@ -32,12 +40,8 @@ func Error(err error) interface{} {
 }
 
 // Success returns a structured sucess message for API responses
-func Success(success ...string) interface{} {
-	response := struct {
-		Success struct {
-			Message string `json:"message"`
-		} `json:"success"`
-	}{}
+func Success(success ...string) successMessage {
+	response := successMessage{}
 	response.Success.Message = "OK"
 	if len(success) > 0 {
 		response.Success.Message = success[0]
@@ -58,14 +62,31 @@ func Debug(w http.ResponseWriter, r *http.Request) {
 // JSON responds with the first non-nil payload, formats error messages
 func JSON(w http.ResponseWriter, responses ...interface{}) {
 	respond := func(payload interface{}) {
-		// json, err := json.Marshal(payload)
-		json, err := json.MarshalIndent(payload, "", "\t")
+		var result []byte
+		var err error
+		encode := func(payload interface{}) ([]byte, error) {
+			// json, err := json.Marshal(payload)
+			return json.MarshalIndent(payload, "", "\t")
+		}
+		switch value := payload.(type) {
+		case errorMessage:
+			// main key is "error"
+			result, err = encode(value)
+		case successMessage:
+			// main key is "success"
+			result, err = encode(value)
+		default:
+			// main key is "response"
+			result, err = encode(struct {
+				Response interface{} `json:"response"`
+			}{value})
+		}
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 		w.Header().Set("Content-Type", "application/json")
-		w.Write(json)
+		w.Write(result)
 	}
 
 	for _, response := range responses {
@@ -83,15 +104,25 @@ func JSON(w http.ResponseWriter, responses ...interface{}) {
 			respond(Error(err))
 		case error:
 			respond(Error(value))
+		case string:
+			if value == "" {
+				continue
+			}
+			respond(value)
+		case bool:
+			if !value {
+				continue
+			}
+			respond(value)
+		case errorMessage:
+			respond(value)
+		case successMessage:
+			respond(value)
 		default:
-			respond(struct {
-				Response interface{} `json:"response"`
-			}{response})
+			respond(value)
 		}
 		// Exit on the first output...
 		return
 	}
-	respond(struct {
-		Response interface{} `json:"response"`
-	}{false})
+	respond(false)
 }
